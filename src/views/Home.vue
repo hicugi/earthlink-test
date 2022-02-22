926cb9c42d4a817122375bf92ebfdc28eba68f6b
Mag <hicugi@gmail.com>
2022-02-22 20:46:10 +0600

    add home header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="home">
-    <br /><br /><br /><br /><br /><br />
+  <div class="p-home">
+    <header class="p-home-header">
+      <button class="p-home-language">يبرع</button>
+    </header>
 
     <LoginBlock />
   </div>
@@ -15,3 +17,47 @@ export default defineComponent({
   components: { LoginBlock }
 })
 </script>
+
+<style lang="scss">
+@import '../scss/colors.scss';
+
+.p-home {
+  &-header {
+    margin-bottom: 84px;
+    padding: 19px 35px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  &-language {
+    border: none;
+    display: flex;
+    background: none;
+    color: $color-primary;
+    font-family: inherit;
+    font-size: 14px;
+    line-height: 16px;
+
+    &::before,
+    &::after {
+      display: block;
+      content: '';
+    }
+    &::before {
+      margin-right: 10px;
+      transform: translateY(2px) rotate(135deg);
+      border: 2px solid;
+      border-color: $color-primary $color-primary transparent transparent;
+      width: 10px;
+      height: 10px;
+    }
+    &::after {
+      margin-left: 8px;
+      width: 20px;
+      height: 20px;
+      background: url(/assets/globe.svg) no-repeat center;
+    }
+  }
+}
+</style>
