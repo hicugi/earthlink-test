dc5bfef3c78170c23128ed292571333b714fef75
Mag <hicugi@gmail.com>
2022-02-22 20:57:01 +0600

    add middleware with user

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,31 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
+import store from '../store'
+
+const homeName = 'home'
+const dashboardName = 'dashboard'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: homeName,
+    component: Home,
+    beforeEnter: () => {
+      if (store.state.user) {
+        return { name: dashboardName }
+      }
+    }
+  },
+  {
+    path: '/dashboard',
+    name: dashboardName,
+    component: Dashboard,
+    beforeEnter: () => {
+      if (!store.state.user) {
+        return { name: homeName }
+      }
+    }
   }
 ]
 
